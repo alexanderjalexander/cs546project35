ff2084e7a14b6df835b6ac0ab78b931155b8fae4
Kian Holden <kian@holden.id.au>
2024-04-11 18:52:23 -0400

    Added some more skeleton code

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,1 +1,14 @@
 //This will be the seed file to add stuff to the database
+import {dbConnection, closeConnection} from '../config/mongoConnection.js';
+import users from '../data/users.js';
+import items from '../data/items.js';
+import dms from '../data/dms.js';
+import trades from '../data/trades.js';
+
+const db = await dbConnection();
+await db.dropDatabase();
+
+//add stuff here
+
+console.log('Done seeding database');
+await closeConnection();
