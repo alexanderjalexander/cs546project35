8db80ed229e6a2d4ae5131121ac3a3343950022f
Kian Holden <kian@holden.id.au>
2024-04-11 19:12:50 -0400

    added a helper file with two methods

---
+++ helpers.js
@@ -0,0 +1,15 @@
+import {ObjectId} from 'mongodb';
+import * as valid from 'valid-url'
+
+export function checkString(str, name) {
+    if (typeof str !== 'string') throw `${name} must be a string`;
+    if (str.trim().length === 0)
+        throw `${name} cannot be an empty string or just spaces`;
+    str = str.trim();
+    return str;
+}
+export function checkIdString(id) {
+    id = checkString(id, 'id')
+    if (!ObjectId.isValid(id)) throw 'invalid object ID';
+    return id;
+}
