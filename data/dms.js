74cda6a33b0af783b8293fc1c43c1ef51c4ef8f8
Kian Holden <kian@holden.id.au>
2024-04-12 13:08:32 -0400

    added more dm collection methods and seed instructions

--- data/dms.js
+++ data/dms.js
@@ -6,7 +6,7 @@ const get = async (id) => {
     /**
      * retrives the DM json by it's Objectid
      *
-     * @param   {Object}  id  the objectid of the DM
+     * @param   {string}  id  the objectid of the DM
      *
      * @return  {Object}      the json of the DM
      */
@@ -14,7 +14,12 @@ const get = async (id) => {
     const dmCollection = await dms();
     const dm = await dmCollection.findOne({_id: new ObjectId(id)});
     if (dm === null) throw 'No product with that id';
+
+    //make these assignments so it returns plain strings instead of objectids
     dm._id = dm._id.toString();
+    dm.actor1 = dm.actor1.toString();
+    dm.actor2 = dm.actor2.toString();
+
     return dm;
   };
 
@@ -22,10 +27,10 @@ const create = async (actor1, actor2) => {
     /**
      * Creates a new Direct Message in the dms collection
      *
-     * @param   {ObjectId}  actor1  Objectid of one messager
-     * @param   {ObjectId}  actor2  Objectid of other messager
+     * @param   {string}  actor1  Objectid of one messager
+     * @param   {string}  actor2  Objectid of other messager
      *
-     * @return  {ObjectId}          Objectid of the direct message
+     * @return  {string}          Objectid of the direct message
      */
     const dmCollection = await dms();
     const newDM = {'actor1':new ObjectId(actor1), 'actor2':new ObjectId(actor1), 'messages': []};
@@ -37,7 +42,54 @@ const create = async (actor1, actor2) => {
     return product;
   };
 
+const writeMsg = async (id, senderid, msg) => {
+    /**
+     * adds a message to the dm found by id
+     *
+     * @param   {string}  id  objectid of the dm
+     * @param   {string}  senderid  objectid of the sender
+     * @param   {string}  msg  sender's message
+     *  
+     * @return  {[type]}      [return description]
+     */
+    id = helper.checkIdString(id);
+    senderid = helper.checkIdString(senderid);
+    msg = helper.checkString(msg);
+    const dmsCollection = await dms();
+    //first we will find the product we need for the new review
+    let dm = await dmsCollection.findOne({_id: new ObjectId(id)});
+
+
+
+    //we need to first find the product, then edit it's contents to include the review
+    let time = new Date(Date.now());
+    const newMsg = {  
+        _id: new ObjectId(),
+        sender: new ObjectId(senderid),
+        content: msg,
+        timestamp: time
+    };
+    
+    dm.messages.push(newMsg);
+    
+    
+    
+    //then we will findOneAndUpdate that same product again but replace it with the new product
+    const updatedInfo = await dmsCollection.findOneAndUpdate(
+        {_id: new ObjectId(id)},
+        {$set: dm},
+        {returnDocument: 'after'}
+    );
+    if (!updatedInfo) {
+        throw 'could not update product successfully';
+    }
+    updatedInfo._id = updatedInfo._id.toString();
+    return updatedInfo;
+}
+
+
 export default {
     get,
-    create
+    create,
+    writeMsg
 };
--- tasks/seed.js
+++ tasks/seed.js
@@ -9,10 +9,16 @@ import dms from '../data/dms.js';
 const db = await dbConnection();
 await db.dropDatabase();
 
-//add stuff here
+//add dummy data here
 const userid1 = new ObjectId();
 const userid2 = new ObjectId();
 const dm1 = await dms.create(userid1.toString(), userid2.toString());
+let msg1 = undefined;
+let msg2 = undefined;
+msg1 = await dms.writeMsg(dm1._id, userid1.toString(), "this is the first message!");
+msg2 = await dms.writeMsg(dm1._id, userid2.toString(), "this is the second message!");
+
+console.log(await dms.get(dm1._id));
 
 console.log('Done seeding database');
 await closeConnection();
