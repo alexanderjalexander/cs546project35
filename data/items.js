233dc85e2cea2e1f40ce7ff705437f98a6439b7c
alexanderjalexander <alexj18082002@gmail.com>
2024-04-13 20:21:53 -0400

    Initial implementation of item db functions

---
+++ data/items.js
@@ -0,0 +1,162 @@
+import {items} from '../config/mongoCollections.js';
+import {ObjectId} from 'mongodb';
+import * as helper from '../helpers.js'
+
+/**@typedef {({_id:ObjectId, userId:ObjectId, name:string, desc:string, price:number, image:string})} item
+ * Describes a tradeable item. _id and userId are strings that can be converted to ObjectId's
+ */
+
+/** 
+ * @returns A complete list of items stored in the database
+ */
+const getAll = async () => {
+    const itemCollection = await items();
+    const items = await itemCollection.find({}).toArray();
+    if (!items) {
+        throw 'Could not fetch all items.'
+    }
+    items = items.map((element) => {
+        element._id = element._id.toString();
+        return element;
+    });
+    return items;
+}
+
+/**
+ * Gets an item from the item collection using an ObjectId
+ * @param {string} id the ObjectID of the item as a string
+ * @returns A singular item.
+ */
+const getById = async (id) => {
+    id = helper.checkIdString(id);
+
+    const itemCollection = await items();
+    const item = await itemCollection.findOne({_id: new ObjectId(id)});
+    if (item === null) {
+        throw `No product with that id has been found.`
+    }
+    item._id = item._id.toString();
+    return item;
+}
+
+/**
+ * Finds all the items owned/listed by a user
+ * @param {string} userId the user's ID as a string
+ * @returns a collection of items belonging to that userId
+ */
+const getAllByUserId = async (userId) => {
+    userId = helper.checkIdString(userId);
+
+    const itemCollection = await items();
+    const items = await itemCollection.find({userId: new ObjectId(userId)}).toArray();
+    if (!items) {
+        throw `No products with that userId has been found.`
+    }
+    return items;
+}
+
+/**
+ * Patch updates an item using at least one of the provided values in updateObject.
+ * @param {string} id the ObjectID of the item as a string
+ * @param {string} userId the ObjectID of the user owning it as a string
+ * @param {{name?:string, desc?:string, price?:number, image?:string}} updateObject 
+ * @returns 
+ */
+const update = async (id, userId, updateObject) => {
+    id = helper.checkIdString(id);
+    userId = helper.checkIdString(userId);
+    if (Object.keys(updateObject).length === 0) {
+        throw `Must supply at least one key/value pair to updateObject.`
+    }
+    if (!Object.keys(updateObject).includes('name')
+        && !Object.keys(updateObject).includes('desc')
+        && !Object.keys(updateObject).includes('price')
+        && !Object.keys(updateObject).includes('image')) {
+        throw `Must supply at least one of these: name, desc, price, image`
+    }
+
+    const item = await getById(id);
+
+    let updatedItem = {
+        userId: item.userId,
+        name: (updateObject.hasOwnProperty('name'))
+            ? helper.checkString(updateObject.name)
+            : item.name,
+        desc: (updateObject.hasOwnProperty('desc'))
+            ? helper.checkString(updateObject.desc)
+            : item.desc,
+        price: (updateObject.hasOwnProperty('price'))
+            ? helper.checkPrice(updateObject.price)
+            : item.price,
+        image: (updateObject.hasOwnProperty('image'))
+            ? helper.checkString(updateObject.image)
+            : item.image
+    };
+
+    const itemCollection = await items();
+    await itemCollection.updateOne(
+        {_id: new ObjectId(updatedItem._id)}, 
+        {$set: updatedItem},
+        {returnDocument: 'after'}
+    );
+
+    if (!updateInfo) throw `Update failed, could not find an item with id of ${id}`;
+    return updateInfo;
+}
+
+/**
+ * Adds a new item to a User's page.
+ * @param {string} userId User's ID represented as a string
+ * @param {string} name The name of the item
+ * @param {string} desc The description of the item
+ * @param {number} price The item's price, must be > 0 with 2 decimal places only
+ * @param {string} image server-side image pathname
+ * @returns the item from the database on successful completion
+ */
+// TODO: either figure out GridFS or test saving it on a server when the time comes to work out backend routes.
+const create = async (userId, name, desc, price, image) => {
+    userId = helper.checkIdString(userId);
+    name = helper.checkString(name, "name");
+    desc = helper.checkString(name, "name");
+    price = helper.checkPrice;
+    image = helper.checkString(name, "name");
+
+    const newItem = {
+        userId, name, desc, price, image
+    }
+
+    const itemCollection = await items();
+    const insertInfo = await itemCollection.insertOne(newItem);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+        throw `Error: could not add item.`;
+    }
+    return await getById(insertInfo.insertedId.toString());
+}
+
+/**
+ * Attempts to remove an item.
+ * @param {string} id Item's ID as a string
+ * @param {string} userId User's ID as a string
+ * @returns Object with id and deleted:true on successful deletion
+ */
+const remove = async (id, userId) => {
+    id = helper.checkIdString(id);
+    userId = helper.checkIdString(userId);
+
+    const itemCollection = await items();
+    const removalInfo = await itemCollection.findOneAndDelete({
+        _id: new ObjectId(id),
+        userId: new ObjectId(userId),
+    });
+
+    if (!removalInfo) {
+        throw `Error: Could not delete product with id of ${id}`;
+    }
+    return {_id: new Object(id), deleted: true}
+}
+
+const exportedMethods = {
+    getAll, getById, getAllByUserId, update, create
+}
+
+export default exportedMethods
--- helpers.js
+++ helpers.js
@@ -12,3 +12,24 @@ export function checkIdString(id) {
     if (!ObjectId.isValid(id)) throw 'invalid object ID';
     return id;
 }
+export function calcDecimalPlaces(num) {
+    if (Math.floor(num) !== num || !Number.isInteger(num)) {
+        let splitnum = num.toString().split('.');
+        return splitnum[1].length;
+    } else {
+        return 0;
+    }
+}
+export function checkPrice(price, varName) {
+    if (price === null || price === undefined) {
+        throw `Error: Provided arg ${varName} doesn't exist`;
+    }
+    if (typeof price !== 'number') {
+        throw `Error: Provided arg ${varName} is not a number. Must be a number >= 0.`;
+    }
+    if (price <= 0) throw `Error: Provided arg ${varName} must be > 0.`
+    if (calcDecimalPlaces(price) > 2) {
+        throw `Error: Provided arg ${varName} must be a price with <= 2 decimal places.`
+    }
+    return price;
+}
