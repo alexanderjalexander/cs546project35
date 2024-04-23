ca9b0c85c546d1ece52a96b09606882de03bb43b
Kian Holden <kian@holden.id.au>
2024-04-23 10:25:09 -0400

    edited route index file

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,10 @@ import authRoutes from './auth_routes.js';
 
 const constructorMethod = (app) => {
     app.use('/', authRoutes);
-    app.use('/dms', productRoutes);
-    app.use('/profiles', reviewRoutes);
+    app.use('/dms', dmRoutes);
+    app.use('/profiles', userRoutes);
     app.use('/items', itemRoutes);
-    app.use('/users', userRoutes);
+    app.use('/trades', tradeRoutes);
 
     app.use('*', (req, res) => {
         return res.status(404).json({error: 'Not found'});
