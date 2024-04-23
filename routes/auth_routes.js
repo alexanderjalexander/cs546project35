c812edc6fae6bbfb300bea0045542a1e3ee9eb3f
Kian Holden <kian@holden.id.au>
2024-04-23 10:24:18 -0400

    more skeleton code for the routes

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,1 +1,8 @@
 //this will have the /login /register etc. routes
+import {Router} from 'express';
+const router = Router();
+import * as help from '../helpers.js';
+
+
+
+export default router;
--- routes/trades.js
+++ routes/trades.js
@@ -1,1 +1,6 @@
 //this will have all the routes that begin with /trades
+import {Router} from 'express';
+const router = Router();
+import * as help from '../helpers.js';
+
+export default router;
