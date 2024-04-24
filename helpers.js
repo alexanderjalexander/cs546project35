0f59bf4ff2b867ce8ab1812c73f399e34695f20c
alexanderjalexander <alexj18082002@gmail.com>
2024-04-24 17:44:00 -0400

    Added extra helper functions

--- helpers.js
+++ helpers.js
@@ -52,3 +52,60 @@ export function makeRate(rating, reviewerName) {
     }
     return `Rating for ${reviewerName} is ${rating}`;
 }
+
+export const checkName = (name) => {
+    name = checkString(name, 'name')
+    if (name.length < 2 || name.length > 25) {
+        throw 'Error: name must be >= 2 and <= 25 characters in length.'
+    }
+    if (/\d/.test(name)) {
+        throw `Error: name cannot contain numbers.`
+    }
+    return name;
+}
+
+export const checkUsername = (username) => {
+    username = checkString(username, 'username');
+    username = username.toLowerCase();
+    if (username.length < 5 || username.length > 20) {
+        throw 'Error: username must be >= 5 and <= 20 characters in length.'
+    }
+    if (/\d/.test(username)) {
+        throw `Error: username cannot contain numbers.`
+    }
+    return username;
+}
+
+export const checkPassword = (password) => {
+    password = checkString(password, 'password');
+    if (/\s/.test(password)) {
+        throw `Error: password cannot contain spaces or whitespace characters.`
+    }
+    if (password.length < 8) {
+        throw `Error: password must contain at least 8 characters.`
+    }
+    if (!(/[A-Z]/).test(password)) {
+        throw `Error: password must contain at least one uppercase letter.`
+    }
+    if (!(/[0-9]/).test(password)) {
+        throw `Error: password must contain at least one number.`
+    }
+    if (!(/[^a-zA-Z0-9]/).test(password)) {
+        throw `Error: password must contain at least one special character.`
+    }
+    return password;
+}
+
+export const checkTheme = (theme) => {
+    if (!theme) {
+        throw `Error: You must supply a theme!`;
+    }
+    if (typeof theme !== 'string') {
+        throw `Error: username must be a string!`;
+    }
+    theme = theme.toLowerCase();
+    if (theme !== 'light' && theme !== 'dark') {
+        throw 'Error: theme is invalid. Must be either \'light\' or \'dark\'.'
+    }
+    return theme;
+}
