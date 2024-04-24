2cdb3ab671070b6d31f3d30c5907775180e9ace9
Kian Holden <kian@holden.id.au>
2024-04-24 16:37:30 -0400

    created home page

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -3,6 +3,25 @@ import {Router} from 'express';
 const router = Router();
 import * as help from '../helpers.js';
 
-
+router.route('/').get(async (req, res) => {
+    console.log("received get request to /");
+    if (req.session.user){ //user logged in
+        return res.render('home', {
+            title: 'Home',
+            nav: [
+                {text: 'login', link: '/login'},
+                {text: 'register', link: '/register'}
+            ]
+        });
+    }
+    //user not logged in
+    return res.render('home', {
+        title: 'Home',
+        nav: [
+            {text: 'login', link: '/login'},
+            {text: 'register', link: '/register'}
+        ]
+    });
+});
 
 export default router;
