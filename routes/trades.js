71eb25bc39d473160984e06d6540b6197db73d7b
Kian Holden <kian@holden.id.au>
2024-04-24 18:14:20 -0400

    skeleton code fro trades routes

--- routes/trades.js
+++ routes/trades.js
@@ -5,10 +5,22 @@ const router = Router();
 import * as help from '../helpers.js';
 
 
-router.route('/').get(async (req, res) => {
-    //this route will log all the trades of the current user.
+router.route('/')
+    .get(async (req, res) => {
+        //this route will log all the trades of the current user.
     
-});
+    })
+    .post(async (req, res) => {
+        //this route will initiate a new trade to another person
+    })
+
+router.route('/:tradeId')
+    .get(async (req, res) => {
+        //views one trade with another user. Offers forms to manage the trade
+    })
+    .patch(async (req, res) => {
+        //updates existing trade, resends the trade to the database and swaps requester/requestee
+    });
 
 
 export default router;
