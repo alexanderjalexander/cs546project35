fb133aa5dd3359684d2d3d42cc93d9ba8f659c7a
Kian Holden <kian@holden.id.au>
2024-04-26 18:28:37 -0400

    added JSON file for seeding

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,10 +1,12 @@
 //This will be the seed file to add stuff to the database
 import { ObjectId } from 'bson';
 import {dbConnection, closeConnection} from '../config/mongoConnection.js';
-// import users from '../data/users.js';
-// import items from '../data/items.js';
+import users from '../data/users.js';
+import items from '../data/items.js';
 import dms from '../data/dms.js';
-// import trades from '../data/trades.js';
+import trades from '../data/trades.js';
+import objectList from './seedData.json' assert { type: "json" };
+
 
 const db = await dbConnection();
 await db.dropDatabase();
@@ -18,7 +20,7 @@ let msg2 = undefined;
 msg1 = await dms.writeMsg(dm1._id, userid1.toString(), "this is the first message!");
 msg2 = await dms.writeMsg(dm1._id, userid2.toString(), "this is the second message!");
 
-console.log(await dms.get(dm1._id));
+console.log(await dms.getById(dm1._id));
 
 console.log('Done seeding database');
 await closeConnection();
