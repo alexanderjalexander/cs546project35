f63b7c5bb4e64e8d0fd2dd5b85a0001704f74508
Kian Holden <kian@holden.id.au>
2024-05-01 13:39:13 -0400

    feat: /trades get route

--- routes/trades.js
+++ routes/trades.js
@@ -4,37 +4,20 @@ import {Router} from 'express';
 const router = Router();
 import * as help from '../helpers.js';
 import {ObjectId} from 'mongodb';
+import tradeData from '../data/trades.js'
+import userData from '../data/users.js'
 
 
 router.route('/')
     .get(async (req, res) => {
         //this route will log all the trades of the current user.
         //this just shows the user they are trading with, along with the status
-        let trades = [
-            {
-                _id: new ObjectId(),
-                status: 'requested',
-                senderId: new ObjectId(),
-                receiverId: new ObjectId(),
-                senderItems: [new ObjectId(), new ObjectId()],
-                receiverItems: [new ObjectId(), new ObjectId()]
-            },
-            {
-                _id: new ObjectId(),
-                status: 'accepted',
-                senderId: new ObjectId(),
-                receiverId: new ObjectId(),
-                senderItems: [new ObjectId(), new ObjectId()],
-                receiverItems: [new ObjectId(), new ObjectId()]
-            },
-        ];
+        let trades = await tradeData.getAll(req.session.user._id)
         //We will build a list of trade logs to render
         //We have a database method that gets all the trades that are either coming from or going to the user
         //we need to parse through all of those trades and then call user database methods to get the username of the other user
         //lets get the usernames from each of those trades now
-        trades = trades.map((element) => {
-            
-        })
+        return res.json(trades);
     })
     .post(async (req, res) => {
         //this route will initiate a new trade to another person
