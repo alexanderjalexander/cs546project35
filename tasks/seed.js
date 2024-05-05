e1567e39c1be8b4906bf1e2709f76cf21984ed4c
Kian Holden <kian@holden.id.au>
2024-05-05 12:36:26 -0400

    deleted commented-out code

--- tasks/seed.js
+++ tasks/seed.js
@@ -15,7 +15,6 @@ const __dirname = path.dirname(__filename)
 
 const jsonFilePath = path.join(__filename, '../seedData.json');
 // Read users.json file 
-// Read asynchronously
 let objectList = undefined;
 try {
     const data = fs.readFileSync(jsonFilePath, 'utf8');
@@ -82,17 +81,6 @@ for(const dm of objectList.DMS){
         await dms.writeMsg(currentDM._id, people[message.sender]._id, message.content)
     }
 }
-
-//add dummy data here
-// const userid1 = new ObjectId();
-// const userid2 = new ObjectId();
-// const dm1 = await dms.create(userid1.toString(), userid2.toString());
-// let msg1 = undefined;
-// let msg2 = undefined;
-// msg1 = await dms.writeMsg(dm1._id, userid1.toString(), "this is the first message!");
-// msg2 = await dms.writeMsg(dm1._id, userid2.toString(), "this is the second message!");
-
-// console.log(await dms.getById(dm1._id));
 
 console.log('Done seeding database');
 await closeConnection();
